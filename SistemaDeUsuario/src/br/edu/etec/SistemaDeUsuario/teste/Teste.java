684e99f0edb7b85eeb26806f236641665950443c
Nicolly Rodrigues <nicollyara0@gmail.com>
2026-05-14 22:01:18 -0300

    Add files via upload

---
+++ SistemaDeUsuario/src/br/edu/etec/SistemaDeUsuario/teste/Teste.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.etec.SistemaDeUsuario.teste;
+
+import br.edu.etec.SistemaDeUsuario.model.NivelDeAcesso;
+import br.edu.etec.SistemaDeUsuario.model.Usuario;
+
+/**
+ *
+ * @author cacal
+ */
+public class Teste {
+    public static void main(String args[]){
+        Usuario usuario = new Usuario("Nicolly", "123", NivelDeAcesso.convidado);
+        usuario.info();
+        usuario.autenticar("123");
+        usuario.getNivel();
+
+    }
+}
